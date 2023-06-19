4fdfdd85e970858ab55d379c9008522a86d89d32
Charlie Zhao <charles.zhao5461@gmail.com>
2023-06-19 12:59:21 +1000

    Add auth with clerk

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+/**
+ * https://clerk.com/docs/nextjs/middleware
+ *
+ * Allows us to have authentication when every
+ * server request is made by embedding the auth
+ * state inside of the request itself.
+ *
+ * The other request we know if the user’s authed
+ * or not based on their cookies, but we don’t know
+ * how to process that without doing it on each
+ * request.  Instead of doing it manually doing it
+ * when req is received, we can do that as part of
+ * middleware on an edge before it ever hits our
+ * own servers.
+ */
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  // https://clerk.com/docs/nextjs/middleware#execution-order-of-before-auth-public-routes-and-after-auth
+  //   afterAuth() {},
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import styles from "./index.module.css";
 import Head from "next/head";
 import Link from "next/link";
@@ -5,6 +6,7 @@ import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
 
   return (
     <>
@@ -14,38 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <p className={styles.showcaseText}>
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+        {!user.isSignedIn && <SignInButton />}
+        {!!user.isSignedIn && <SignOutButton />}
       </main>
     </>
   );
