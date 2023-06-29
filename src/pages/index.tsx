e4230acaba050acf5215463ce8dfadb5688f8c86
Charlie Zhao <charles.zhao5461@gmail.com>
2023-06-29 22:42:49 +1000

    frontend: allow image upload when creating studySpot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import type { FormEvent } from "react";
+import { useDropzone } from "react-dropzone";
 import { api } from "~/utils/api";
+import { uploadImagesToS3UsingPresignedUrls } from "~/utils/client-helpers";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -9,22 +11,53 @@ export default function Home() {
   const [lat, setLat] = useState(0);
   const [lng, setLng] = useState(0);
 
-  const studySpots = api.studySpots.getAll.useQuery();
-  const { mutate, error } = api.studySpots.createOne.useMutation();
+  const studySpots = api.studySpots.getNotValidated.useQuery();
+  const apiUtils = api.useContext();
+  const { mutate, error } = api.studySpots.createOne.useMutation({
+    onSuccess: () => apiUtils.studySpots.getNotValidated.invalidate(),
+  });
   const nameError = error?.data?.zodError?.fieldErrors.name;
   const errorMessage = error?.message;
 
+  const { mutate: getPresignedUrls } = api.s3.getPresignedUrls.useMutation({
+    onSuccess: async (presignedUrls) => {
+      if (!presignedUrls.length) return;
+
+      const imageUrls = await uploadImagesToS3UsingPresignedUrls({
+        presignedUrls: presignedUrls,
+        acceptedFiles: acceptedFiles,
+      });
+
+      mutate({
+        name,
+        hasWifi,
+        imageUrls,
+        location: {
+          lat,
+          lng,
+        },
+      });
+    },
+  });
+
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({
+      maxFiles: 8,
+      maxSize: 4000000, // 4mb
+    });
+
+  const files = useMemo(() => {
+    return acceptedFiles.map((file) => (
+      <li key={file.name}>
+        {file.name} - {file.size} bytes
+      </li>
+    ));
+  }, [acceptedFiles]);
+
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    mutate({
-      name,
-      hasWifi,
-      location: {
-        lat,
-        lng,
-      },
-    });
+    const filesToSubmit = acceptedFiles.map((file) => file.type);
+    getPresignedUrls({ contentTypes: filesToSubmit });
   };
 
   return (
@@ -50,7 +83,6 @@ export default function Home() {
               />
             </label>
           </div>
-
           <div>
             <label>
               <input
@@ -61,7 +93,6 @@ export default function Home() {
             </label>
             hasWifi
           </div>
-
           <div>
             <label>
               lat:{" "}
@@ -72,7 +103,6 @@ export default function Home() {
               />
             </label>
           </div>
-
           <div>
             <label>
               lng:{" "}
@@ -83,6 +113,30 @@ export default function Home() {
               />
             </label>
           </div>
+
+          <div
+            {...getRootProps()}
+            className="dropzone-container"
+            style={{ width: `200px`, height: `200px`, background: `lightgray` }}
+          >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <div className="flex h-full items-center justify-center font-semibold">
+                <p>Drop the file here...</p>
+              </div>
+            ) : (
+              <div className="flex h-full items-center justify-center font-semibold">
+                <p>Drag n drop file here, or click to select files</p>
+              </div>
+            )}
+          </div>
+          <aside className="my-2">
+            <h4 className="font-semibold text-zinc-400">
+              Files pending upload:
+            </h4>
+            <ul>{files}</ul>
+          </aside>
+
           <input type="submit" />
         </form>
 
@@ -93,12 +147,62 @@ export default function Home() {
         <hr />
         <div>
           <h2>Study Spots</h2>
-          {studySpots.data?.map((studySpot) => (
-            <div key={studySpot.id}>
-              <div>has wifi: {studySpot.hasWifi}</div>
-              <div>name: {studySpot.name}</div>
-            </div>
-          ))}
+          <div
+            style={{
+              display: `grid`,
+              gridTemplateColumns: `repeat(3,1fr)`,
+              gap: `1rem`,
+            }}
+          >
+            {studySpots.data?.map((studySpot) => (
+              <div
+                key={studySpot.id}
+                style={{
+                  outline: `1px solid #333`,
+                  background: `#f8f8f8`,
+                }}
+              >
+                <div style={{ padding: `1rem` }}>
+                  <div
+                    style={{
+                      fontSize: `3rem`,
+                      fontWeight: 600,
+                      fontFamily: "fantasy",
+                      letterSpacing: "-0.04em",
+                    }}
+                  >
+                    {studySpot.name}
+                  </div>
+                  <div>has wifi: {studySpot.hasWifi}</div>
+                  <div>
+                    lat: {studySpot.location.lat}, long:{" "}
+                    {studySpot.location.lng}
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: `flex`,
+                    flexWrap: `wrap`,
+                    borderTop: `1px dashed black`,
+                    padding: `1rem`,
+                  }}
+                >
+                  {studySpot.images.map((image) => (
+                    <img
+                      src={image.url}
+                      key={image.url}
+                      alt="study spot"
+                      style={{
+                        maxWidth: `200px`,
+                        width: `100%`,
+                        alignSelf: "center",
+                      }}
+                    />
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
