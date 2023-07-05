7176dff9a051e62437a17321631edefae805cd3b
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-05 21:49:59 +1000

    Install tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-import "~/styles/globals.css";
+import "~/styles/global.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Header from "~/components/Header";
 
