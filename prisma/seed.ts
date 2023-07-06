dc99df45e18d239211fe0ea766cc42fa3626ae01
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-06 23:32:54 +1000

    Update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,32 @@
 import { prisma } from "../src/server/db";
+import slugify from "slugify";
 
 async function main() {
-  await prisma.studySpot.deleteMany();
-  await prisma.location.deleteMany();
-  await prisma.image.deleteMany();
+  /** 
+   * Uncomment this to delete all data in the database
+    await prisma.studySpot.deleteMany();
+    await prisma.location.deleteMany();
+    await prisma.image.deleteMany();
+  */
+
+  const allStudySpots = await prisma.studySpot.findMany();
+  const allStudySpotsNames = allStudySpots.map((s) => s.name);
+
+  // Add slug to each study spot
+  allStudySpotsNames.forEach(async (name) => {
+    await prisma.studySpot.update({
+      where: {
+        name,
+      },
+      data: {
+        slug: slugify(name, {
+          remove: /[*+~.()'"!:@]/g,
+          lower: true,
+          strict: true,
+        }),
+      },
+    });
+  });
 }
 
 main()
