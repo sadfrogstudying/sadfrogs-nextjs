bf9f4edcc32a8155ac1512e4b3d5e10fea91f663
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-10 01:00:19 +1000

    Add skeletons and make individual study spot page completely CSR

--- src/components/StudySpot/Hero/Text.tsx
+++ src/components/StudySpot/Hero/Text.tsx
@@ -1,4 +1,5 @@
 import { Prisma } from "@prisma/client";
+import { Skeleton } from "~/components/UI/Skeleton";
 
 type StudySpotExtended = Prisma.StudySpotGetPayload<{
   include: {
@@ -6,7 +7,7 @@ type StudySpotExtended = Prisma.StudySpotGetPayload<{
   };
 }>;
 
-const HeroText = ({ studySpot }: { studySpot: StudySpotExtended }) => {
+const HeroText = ({ studySpot }: { studySpot?: StudySpotExtended }) => {
   const {
     createdAt,
     hasWifi,
@@ -17,25 +18,31 @@ const HeroText = ({ studySpot }: { studySpot: StudySpotExtended }) => {
     slug,
     updatedAt,
     location,
-  } = studySpot;
-
-  const studySpotMapped = Object.entries({
-    hasWifi: hasWifi,
-    id,
-    isValidated: isValidated,
-    slug,
-  });
+  } = studySpot || {};
 
   return (
     <>
       <div className="w-full h-fit p-4 space-y-4 md:w-96">
-        <h1 className="text-3xl font-serif">{name}</h1>
-        <p className="w-full">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          excepturi assumenda quas dolorum similique, veniam doloremque earum
-          sit voluptate quo vero nulla, mollitia adipisci facere explicabo
-          recusandae repudiandae.
-        </p>
+        {name ? (
+          <h1 className="text-3xl font-serif">{name}</h1>
+        ) : (
+          <Skeleton className="h-8 w-4/5" />
+        )}
+        {name ? (
+          <p className="w-full">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            excepturi assumenda quas dolorum similique, veniam doloremque earum
+            sit voluptate quo vero nulla, mollitia adipisci facere explicabo
+            recusandae repudiandae.
+          </p>
+        ) : (
+          <div className="space-y-2">
+            <Skeleton className="h-4" />
+            <Skeleton className="h-4" />
+            <Skeleton className="h-4" />
+            <Skeleton className="h-4" />
+          </div>
+        )}
       </div>
     </>
   );
--- src/pages/study-spot/[slug].tsx
+++ src/pages/study-spot/[slug].tsx
@@ -1,37 +1,41 @@
-import type { Prisma } from "@prisma/client";
-import axios from "axios";
-import type { GetStaticPaths, GetStaticProps } from "next";
 import VerticalCarousel from "~/components/StudySpot/Hero/VerticalCarousel";
 import HeroText from "~/components/StudySpot/Hero/Text";
 import InfoTable from "~/components/StudySpot/Info/Table";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
 
-type StudySpotComplete = Prisma.StudySpotGetPayload<{
-  include: {
-    location: true;
-    images: true;
-  };
-}>;
+const StudySpotPage = () => {
+  const router = useRouter();
+  const slug = typeof router.query.slug === "string" ? router.query.slug : "";
 
-const StudySpotPage = ({ studySpot }: { studySpot: StudySpotComplete }) => {
+  const studySpot = api.studySpots.getOne.useQuery(
+    {
+      slug,
+    },
+    {
+      enabled: !!slug,
+    }
+  );
+
   const {
     createdAt,
     hasWifi,
     id,
     isValidated,
     locationId,
-    name,
-    slug,
+    name = "",
     updatedAt,
-    images,
+    images = [],
     location,
-  } = studySpot;
+  } = studySpot.data || {};
 
   return (
     <main className="pb-4 h-fit">
       <section className="pt-12 flex flex-wrap items-end min-h-screen md:pt-0">
-        <HeroText studySpot={studySpot} />
-        <VerticalCarousel name={name} images={images} />
-        {/* <HeroCarousel name={name} images={images} /> */}
+        <>
+          <HeroText studySpot={studySpot.data} />
+          <VerticalCarousel name={name} images={images} />
+        </>
       </section>
       <section className="pb-4 flex flex-col justify-end align-start items-start min-h-screen w-full">
         <InfoTable />
@@ -41,39 +45,3 @@ const StudySpotPage = ({ studySpot }: { studySpot: StudySpotComplete }) => {
 };
 
 export default StudySpotPage;
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { slug = "" } = context.params!;
-
-  const { data } = await axios.get<StudySpotComplete>(
-    "http://localhost:3000/api/studyspots.getOne",
-    {
-      params: {
-        slug: slug,
-      },
-    }
-  );
-
-  return {
-    props: {
-      studySpot: data,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await axios.get<string[]>(
-    "http://localhost:3000/api/studyspots.getAllPaths"
-  );
-
-  return {
-    paths: data.map((path) => {
-      return { params: { slug: path } };
-    }),
-    fallback: false,
-  };
-};
