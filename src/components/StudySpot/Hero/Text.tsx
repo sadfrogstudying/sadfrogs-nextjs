1c629beaa36c6786d3a6d99f4b6160f17e814a6d
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-10 16:42:28 +1000

    Lint

--- src/components/StudySpot/Hero/Text.tsx
+++ src/components/StudySpot/Hero/Text.tsx
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import { type Prisma } from "@prisma/client";
 import { Skeleton } from "~/components/UI/Skeleton";
 
 type StudySpotExtended = Prisma.StudySpotGetPayload<{
