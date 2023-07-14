27ffe54d6f53680a6cd52459dfba0d0dc48f2979
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-14 14:51:54 +1000

    Refactor google places api into hook

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from "react";
 import { Check } from "lucide-react";
 
 import { cn } from "~/lib/utils";
@@ -8,154 +7,21 @@ import {
   CommandInput,
   CommandItem,
 } from "~/components/UI/Command";
-import useDebounce from "~/hooks/useDebounce";
 import Script from "next/script";
 import { env } from "~/env.mjs";
-
-type AutocompletePrediction = google.maps.places.QueryAutocompletePrediction;
-type AutocompleteService = google.maps.places.AutocompleteService;
-type AutocompleteSessionToken = google.maps.places.AutocompleteSessionToken;
-type PlacesService = google.maps.places.PlacesService;
-type PlaceResult = google.maps.places.PlaceResult;
-type PlaceResultPicked = Pick<
-  PlaceResult,
-  | "address_components"
-  | "opening_hours"
-  | "formatted_address"
-  | "geometry"
-  | "name"
-  | "opening_hours"
-  | "website"
->;
-
-type CachedData = Record<
-  string,
-  { data: AutocompletePrediction[]; maxAge: number }
->;
-
-const cacheKey = "sfgpk"; // Key to identify our data in sessionStorage
-const cache = 0.25 * 60 * 60; // 15 minutes in seconds
-
-const getCachedData = (): CachedData => {
-  return JSON.parse(sessionStorage.getItem(cacheKey) || "{}") as CachedData;
-};
+import useGooglePlaces from "~/hooks/useGooglePlaces";
 
 const LocationSearchInput = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [predictions, setPredictions] = useState<AutocompletePrediction[]>([]);
-  const [selectedPlace, setSelectedPlace] = useState<PlaceResultPicked | null>(
-    null
-  );
-
-  const [scriptReady, setScriptReady] = useState(false);
-  const [libraryReady, setLibraryReady] = useState(false);
-
-  const serviceAutocomplete = useRef<AutocompleteService>();
-  const servicePlaces = useRef<PlacesService>();
-  const sessionToken = useRef<AutocompleteSessionToken | null>(null);
-
-  const placesDivRef = useRef<HTMLDivElement>(null); // need to figure out what this is for
-
-  const refreshSessionToken = () => {
-    sessionToken.current = new google.maps.places.AutocompleteSessionToken();
-  };
-
-  useEffect(() => {
-    if (!scriptReady || !placesDivRef.current) return;
-
-    serviceAutocomplete.current =
-      new window.google.maps.places.AutocompleteService();
-    servicePlaces.current = new window.google.maps.places.PlacesService(
-      placesDivRef.current
-    );
-
-    refreshSessionToken();
-
-    setLibraryReady(true);
-  }, [scriptReady]);
-
-  const getPredictions = useDebounce(() => {
-    if (!serviceAutocomplete.current || !sessionToken.current) return;
-
-    void serviceAutocomplete.current.getPlacePredictions(
-      { sessionToken: sessionToken.current, input: inputValue },
-      (predictions, status) => {
-        if (status !== "OK") return alert(status);
-
-        setPredictions(predictions || []);
-
-        // adding new predictions to cache
-        const cachedData = getCachedData();
-        cachedData[inputValue] = {
-          data: predictions || [],
-          maxAge: Date.now() + cache * 1000,
-        };
-        sessionStorage.setItem(cacheKey, JSON.stringify(cachedData));
-      }
-    );
-  });
-
-  const onChange = (val: string) => {
-    setInputValue(val);
-
-    if (val === "") return setPredictions([]);
-
-    // check cache
-    const cachedData = getCachedData();
-    // remove expired cached data
-    const reducedCachedData = Object.keys(cachedData).reduce(
-      (acc: CachedData, key) => {
-        const entry = cachedData[key];
-        if (entry && entry.maxAge - Date.now() >= 0) acc[key] = entry;
-        return acc;
-      },
-      {} as CachedData
-    );
-    const cachedPredictions = reducedCachedData[val]?.data || null;
-    if (cachedPredictions) return setPredictions(cachedPredictions);
-
-    // if not in cache, send request
-    getPredictions();
-  };
-
-  const onSelect = (desc: string) => {
-    const selectedPlaceId = predictions.find(
-      (p) => p.description.toLowerCase().trim() === desc
-    )?.place_id;
-
-    const confirmed = confirm(
-      "Do you want to getDetails and complete the session?"
-    );
-
-    if (
-      !confirmed ||
-      !selectedPlaceId ||
-      !servicePlaces.current ||
-      !sessionToken.current
-    )
-      return;
-
-    return servicePlaces.current.getDetails(
-      {
-        sessionToken: sessionToken.current,
-        placeId: selectedPlaceId,
-        fields: [
-          "address_components",
-          "opening_hours",
-          "formatted_address",
-          "geometry",
-          "name",
-          "opening_hours",
-          "website",
-        ],
-      },
-      (place, status) => {
-        if (status !== "OK") return alert(status);
-        setSelectedPlace(place);
-        refreshSessionToken();
-      }
-    );
-  };
+  const {
+    setScriptReady,
+    libraryReady,
+    placesDivRef,
+    onChange,
+    onSelect,
+    inputValue,
+    predictions,
+    selectedPlace,
+  } = useGooglePlaces();
 
   return (
     <div>
@@ -166,7 +32,7 @@ const LocationSearchInput = () => {
         }}
       />
       <div ref={placesDivRef}></div>
-      <div className="w-96 border rounded p-2">
+      <div className="w-96 border rounded-md p-2 w-full">
         <Command shouldFilter={false}>
           <CommandInput
             placeholder={!libraryReady ? "Loading..." : "Search location..."}
---
+++ src/hooks/useGooglePlaces.ts
@@ -0,0 +1,162 @@
+import { useEffect, useRef, useState } from "react";
+
+import useDebounce from "~/hooks/useDebounce";
+
+type AutocompletePrediction = google.maps.places.QueryAutocompletePrediction;
+type AutocompleteService = google.maps.places.AutocompleteService;
+type AutocompleteSessionToken = google.maps.places.AutocompleteSessionToken;
+type PlacesService = google.maps.places.PlacesService;
+type PlaceResult = google.maps.places.PlaceResult;
+type PlaceResultPicked = Pick<
+  PlaceResult,
+  | "address_components"
+  | "opening_hours"
+  | "formatted_address"
+  | "geometry"
+  | "name"
+  | "opening_hours"
+  | "website"
+>;
+
+type CachedData = Record<
+  string,
+  { data: AutocompletePrediction[]; maxAge: number }
+>;
+
+const cacheKey = "sfgpk"; // Key to identify our data in sessionStorage
+const cacheMaxAge = 0.25 * 60 * 60; // 15 minutes in seconds
+
+const getCachedData = (): CachedData => {
+  return JSON.parse(sessionStorage.getItem(cacheKey) || "{}") as CachedData;
+};
+
+const useGooglePlaces = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [predictions, setPredictions] = useState<AutocompletePrediction[]>([]);
+  const [selectedPlace, setSelectedPlace] = useState<PlaceResultPicked | null>(
+    null
+  );
+
+  const [scriptReady, setScriptReady] = useState(false);
+  const [libraryReady, setLibraryReady] = useState(false);
+
+  const serviceAutocomplete = useRef<AutocompleteService>();
+  const servicePlaces = useRef<PlacesService>();
+  const sessionToken = useRef<AutocompleteSessionToken | null>(null);
+
+  const placesDivRef = useRef<HTMLDivElement>(null); // need to figure out what this is for
+
+  const refreshSessionToken = () => {
+    sessionToken.current = new google.maps.places.AutocompleteSessionToken();
+  };
+
+  useEffect(() => {
+    if (!scriptReady || !placesDivRef.current) return;
+
+    serviceAutocomplete.current =
+      new window.google.maps.places.AutocompleteService();
+    servicePlaces.current = new window.google.maps.places.PlacesService(
+      placesDivRef.current
+    );
+
+    refreshSessionToken();
+
+    setLibraryReady(true);
+  }, [scriptReady]);
+
+  const getPredictions = useDebounce(() => {
+    if (!serviceAutocomplete.current || !sessionToken.current) return;
+
+    void serviceAutocomplete.current.getPlacePredictions(
+      { sessionToken: sessionToken.current, input: inputValue },
+      (predictions, status) => {
+        if (status !== "OK") return alert(status);
+
+        setPredictions(predictions || []);
+
+        // adding new predictions to cache
+        const cachedData = getCachedData();
+        cachedData[inputValue] = {
+          data: predictions || [],
+          maxAge: Date.now() + cacheMaxAge * 1000,
+        };
+        sessionStorage.setItem(cacheKey, JSON.stringify(cachedData));
+      }
+    );
+  });
+
+  const onChange = (val: string) => {
+    setInputValue(val);
+
+    if (val === "") return setPredictions([]);
+
+    // check cache
+    const cachedData = getCachedData();
+    // remove expired cached data
+    const reducedCachedData = Object.keys(cachedData).reduce(
+      (acc: CachedData, key) => {
+        const entry = cachedData[key];
+        if (entry && entry.maxAge - Date.now() >= 0) acc[key] = entry;
+        return acc;
+      },
+      {} as CachedData
+    );
+    const cachedPredictions = reducedCachedData[val]?.data || null;
+    if (cachedPredictions) return setPredictions(cachedPredictions);
+
+    // if not in cache, send request
+    getPredictions();
+  };
+
+  const onSelect = (desc: string) => {
+    const selectedPlaceId = predictions.find(
+      (p) => p.description.toLowerCase().trim() === desc
+    )?.place_id;
+
+    const confirmed = confirm(
+      "Do you want to getDetails and complete the session?"
+    );
+
+    if (
+      !confirmed ||
+      !selectedPlaceId ||
+      !servicePlaces.current ||
+      !sessionToken.current
+    )
+      return;
+
+    return servicePlaces.current.getDetails(
+      {
+        sessionToken: sessionToken.current,
+        placeId: selectedPlaceId,
+        fields: [
+          "address_components",
+          "opening_hours",
+          "formatted_address",
+          "geometry",
+          "name",
+          "opening_hours",
+          "website",
+        ],
+      },
+      (place, status) => {
+        if (status !== "OK") return alert(status);
+        setSelectedPlace(place);
+        refreshSessionToken();
+      }
+    );
+  };
+
+  return {
+    setScriptReady,
+    libraryReady,
+    placesDivRef,
+    onChange,
+    onSelect,
+    inputValue,
+    predictions,
+    selectedPlace,
+  };
+};
+
+export default useGooglePlaces;
