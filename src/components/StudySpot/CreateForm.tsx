6cbf8af6889b89a61a6bb54f5210ece7563314f6
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-19 11:00:38 +0900

    Refactor inputs out of CreateForm

--- src/components/StudySpot/CreateForm.tsx
+++ src/components/StudySpot/CreateForm.tsx
@@ -1,25 +1,21 @@
 import { Button } from "~/components/UI/Button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/components/UI/Form";
-import { Input } from "~/components/UI/Input";
+import { Form } from "~/components/UI/Form";
 import { useForm } from "react-hook-form";
-import { Checkbox } from "../UI/Checkbox";
-import FileInput from "../UI/FileInput";
 import { api } from "~/utils/api";
 import {
   parseZodClientError,
   uploadImagesToS3UsingPresignedUrls,
 } from "~/utils/helpers";
-import LocationSearchInput from "../LocationSearch";
 import { StudySpotInputV2 } from "~/schemas/study-spots";
+import CreateFormGeneral from "./CreateFormGeneral";
 
+/**
+ *
+ * This component is single source of truth for form data and handles submission.
+ * Input fields are rendered in children.
+ * Eventually error logic and component will be refactored out.
+ *
+ */
 const CreateStudySpotForm = ({ onSuccess }: { onSuccess?: () => void }) => {
   const form = useForm<StudySpotInputV2>({
     defaultValues: {
@@ -68,14 +64,6 @@ const CreateStudySpotForm = ({ onSuccess }: { onSuccess?: () => void }) => {
         acceptedFiles: images,
       });
 
-      console.log(name);
-      console.log(rating);
-      console.log(wifi);
-      console.log(powerOutlets);
-      console.log(noiseLevel);
-      console.log(venueType);
-      console.log(imageUrls);
-      return;
       createStudySpot({
         name,
         rating,
@@ -103,13 +91,12 @@ const CreateStudySpotForm = ({ onSuccess }: { onSuccess?: () => void }) => {
     },
   });
 
-  const submitHandler = form.handleSubmit((data) =>
-    checkIfNameExists({ name: data.name })
-  );
+  const submitHandler = form.handleSubmit((data) => {
+    checkIfNameExists({ name: data.name });
+  });
 
   const isLoading = createIsLoading || getUrlsIsLoading || nameExistsLoading;
-  const submitDisabled =
-    isLoading || !form.watch("images").length || !form.watch("name");
+  const submitDisabled = isLoading || !form.watch("images").length;
   const zodErrorMessages = parseZodClientError(createError?.data?.zodError);
 
   return (
@@ -121,138 +108,7 @@ const CreateStudySpotForm = ({ onSuccess }: { onSuccess?: () => void }) => {
         }}
         className="space-y-8 m-auto"
       >
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter Name" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="rating"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Rating</FormLabel>
-              <FormControl>
-                <Input placeholder="0" {...field} type="number" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="wifi"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-center space-x-3 space-y-0 rounded-md border p-4">
-              <FormControl>
-                <Checkbox
-                  checked={field.value}
-                  onCheckedChange={(v) => form.setValue("wifi", !!v)}
-                />
-              </FormControl>
-              <div className="space-y-1 leading-none">
-                <FormLabel>Wifi</FormLabel>
-                <FormDescription>
-                  Does this study spot have wifi?
-                </FormDescription>
-              </div>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="powerOutlets"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-              <FormControl>
-                <Checkbox
-                  checked={field.value}
-                  onCheckedChange={(v) => form.setValue("powerOutlets", !!v)}
-                />
-              </FormControl>
-              <div className="space-y-1 leading-none">
-                <FormLabel>Power Outlets</FormLabel>
-                <FormDescription>
-                  Does this spot have power outlets?
-                </FormDescription>
-              </div>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="noiseLevel"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Noise Level</FormLabel>
-              <FormControl>
-                <Input placeholder="Loud at peak hours" {...field} />
-              </FormControl>
-              <FormDescription>How is the noise level?</FormDescription>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="venueType"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Venue Type</FormLabel>
-              <FormControl>
-                <Input placeholder={`Cafe`} {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="images"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Images</FormLabel>
-              <FormControl>
-                <FileInput
-                  isSuccess={false}
-                  setValue={(files) => form.setValue("images", files)}
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>Images you want to upload</FormDescription>
-            </FormItem>
-          )}
-        />
-        {/* <FormField
-          control={form.control}
-          name="location"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Location</FormLabel>
-                <FormControl>
-                  <LocationSearchInput />
-                </FormControl>
-                <FormDescription>Location of the study spot</FormDescription>
-              </FormItem>
-            );
-          }}
-        /> */}
-
+        <CreateFormGeneral form={form} />
         <ul className="text-sm text-destructive">
           {zodErrorMessages.length !== 0 ? (
             <>
--- src/pages/create-spot.tsx
+++ src/pages/create-spot.tsx
@@ -4,20 +4,15 @@ import { Separator } from "~/components/UI/Seperator";
 const CreateSpotPage = () => {
   return (
     <div className="p-4 pt-36">
-      <section className="max-w-2xl m-auto border rounded-md p-8">
+      <section className="max-w-4xl m-auto border rounded-md p-8">
         <div className="space-y-0.5 p-30">
-          <h2 className="text-2xl font-bold tracking-tight">Create New Spot</h2>
-          <p className="text-muted-foreground">Create a new study spot.</p>
+          <h2 className="text-2xl font-bold tracking-tight">
+            Create New Study Spot
+          </h2>
         </div>
         <Separator className="my-6" />
-        <div className="space-y-6">
-          <div>
-            <h3 className="text-lg font-medium">General</h3>
-            <p className="text-sm text-muted-foreground">
-              This section must be filled out.
-            </p>
-          </div>
-          <Separator />
+
+        <div className="w-full space-y-4 ml-4">
           <CreateStudySpotForm />
         </div>
       </section>
