ebdd3dc5fb518ba067c1973c46053b6106db4ca7
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-20 20:34:59 +0900

    Style headaer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,14 @@ const Header = () => {
     <>
       <header className="fixed flex p-4 z-20 md:w-auto md:bg-white rounded">
         <Link href="/">
-          <h1 className="text-2xl font-serif font-normal tracking-tight md:text-4xl">
+          <h1 className="text-2xl font-serif tracking-tight md:text-4xl">
             Sad Frogs Studying
           </h1>
         </Link>
       </header>
       <div
         role="menubar"
-        className="flex fixed top-0 right-0 p-4 z-20 gap-4 justify-center items-center"
+        className="flex fixed top-0 right-0 p-4 z-20 gap-4 justify-center items-center font-mono"
       >
         <Link href="/about" className="h-fit">
           About
