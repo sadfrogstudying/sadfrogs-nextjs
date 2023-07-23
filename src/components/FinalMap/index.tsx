89cfe24158ea15aed172ea5e07e5dde6af744357
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-23 13:51:24 +0900

    Fix map skele

--- src/components/FinalMap/index.tsx
+++ src/components/FinalMap/index.tsx
@@ -9,10 +9,7 @@ const FinalMap = dynamic(
   () => import("~/components/FinalMap/FinalDynamicMap"),
   {
     loading: () => (
-      <Skeleton
-        className="w-full mb-4 rounded-md overflow-hidden border border-gray-200 flex justify-center items-center font-mono bg-gray-300"
-        style={{ height: `500px` }}
-      >
+      <Skeleton className="w-full h-96 mb-4 rounded-md overflow-hidden border border-gray-200 flex justify-center items-center font-mono bg-gray-300">
         Loading Map...
       </Skeleton>
     ),
