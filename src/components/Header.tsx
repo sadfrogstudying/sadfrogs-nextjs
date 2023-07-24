204eb9ea68d7a43ad2e7b14380a55f77686055a0
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-24 13:55:00 +0900

    Hide header on mappage, and adjust spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import dynamic from "next/dynamic";
 import { Button } from "./UI/Button";
@@ -17,14 +18,21 @@ const Loading = () => (
 );
 
 const Header = () => {
+  const pathname = usePathname();
+  const isMapPage = pathname === "/map";
+
   return (
     <header className="pointer-events-none fixed flex justify-between z-20 w-full rounded top-0 left-0">
-      <h1 className="pointer-events-auto text-2xl font-serif tracking-tight md:text-4xl flex justify-center items-center p-4">
+      <h1
+        className={`pointer-events-auto text-3xl font-serif tracking-tight md:text-4xl flex justify-center items-center p-4 bg-white rounded-md m-0 md:m-4 ${
+          isMapPage ? "md:invisible" : "visible"
+        }`}
+      >
         <Link href="/">Sad Frogs Studying</Link>
       </h1>
       <div
         role="menubar"
-        className="flex p-4 z-20 gap-4 justify-center items-center font-mono"
+        className="flex p-4 z-20 gap-4 justify-center items-center font-mono md:m-4 rounded-md"
       >
         <Link href="/about" className="pointer-events-auto h-fit">
           About
