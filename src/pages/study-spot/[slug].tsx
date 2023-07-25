abbf1a64a10421d237c6cba3c9fae0e3f15165fa
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-25 11:59:06 +0900

    Remove Hero Text

--- src/pages/study-spot/[slug].tsx
+++ src/pages/study-spot/[slug].tsx
@@ -1,10 +1,10 @@
-import HeroText from "~/components/StudySpot/Hero/Text";
 import InfoTable from "~/components/StudySpot/Info/Table";
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
 
 import dynamic from "next/dynamic";
 import FullWidthCarouselSkeleton from "~/components/StudySpot/Hero/FullWidthCarouselSkeleton";
+import { Skeleton } from "~/components/UI/Skeleton";
 
 const FullWidthHeroCarousel = dynamic(
   () => import("~/components/StudySpot/Hero/FullWidthCarousel"),
@@ -27,12 +27,31 @@ const StudySpotPage = () => {
     }
   );
 
-  const { name = "", images = [] } = studySpot.data || {};
+  const { name = "", images = [], address = "" } = studySpot.data || {};
 
   return (
     <main className="pb-4 h-fit">
       <section className="flex-col-reverse pt-12 min-h-screen md:pt-0 mt-auto flex flex-col">
-        <HeroText studySpot={studySpot.data} />
+        <div className="w-full h-fit p-4 space-y-4 md:w-96">
+          {name ? (
+            <>
+              <h1 className="text-3xl font-serif">{name}</h1>
+              <div className="italic font-mono">
+                {address
+                  ? address
+                  : "No address yet, submit one to help others find this spot!"}
+              </div>
+            </>
+          ) : (
+            <>
+              <Skeleton className="h-9 w-4/5 mt-2" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-1/2" />
+              </div>
+            </>
+          )}
+        </div>
         <div>
           <FullWidthHeroCarousel images={images} name={name} />
         </div>
