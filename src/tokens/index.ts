dc09b3da326551d35f38a8b028039aa0377076f4
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-31 14:37:25 +0900

    Remove undeeded file

--- src/tokens/index.ts
+++
@@ -1,9 +0,0 @@
-export const formTokens = {
-  borderRadius: 0.5,
-  height: 2.5,
-  inputSpacing: 10,
-  lineHeight: 1.4,
-  labelSpacing: 0.5,
-  inputBg: "#fff",
-  borderColor: "#d8d8d8",
-} as const;
