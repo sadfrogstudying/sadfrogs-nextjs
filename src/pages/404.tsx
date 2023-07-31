055455b050608835a4a2459a942cde124f661510
Charlie Zhao <charles.zhao5461@gmail.com>
2023-07-31 14:41:00 +0900

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <main className="p-4 h-full w-full flex justify-center items-center">
+      <h1 className="font-mono">404 - Page Not Found</h1>
+    </main>
+  );
+}
