3b82f9d56e2b6d580fbeebe13b3c3b3f291376e3
Charlie Zhao <charles.zhao5461@gmail.com>
2023-10-10 14:08:24 +0900

    Handle user denying auth access

--- src/pages/api/auth/[auth0].ts
+++ src/pages/api/auth/[auth0].ts
@@ -1,3 +1,15 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleCallback } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default handleAuth();
+const authOptions = {
+  async callback(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      await handleCallback(req, res);
+    } catch (error) {
+      console.log(error);
+      res.redirect("/");
+    }
+  },
+};
+
+export default handleAuth(authOptions);
